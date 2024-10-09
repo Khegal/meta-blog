f27a599ba98392a7bf1f658b79ab322c8a937aa8
Khegal <Khegal95@gmail.com>
2024-10-09 18:25:46 +0800

    main

--- components/Trending.tsx
+++ components/Trending.tsx
@@ -2,6 +2,7 @@ const Trending = () => {
   return (
     <div className="flex gap-[30px]">
       <p className="font-bold font-2xl leading-7">Trending</p>
+      <div className="flex justify-between "></div>
     </div>
   );
 };
