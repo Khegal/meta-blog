3f82c2a100ff05afc8b25822506bcc6d57741f21
Khegal <khegal@Khegals-MacBook-Air.local>
2024-10-23 14:37:14 +0800

    main

--- app/ContactUs/page.tsx
+++ app/ContactUs/page.tsx
@@ -1,13 +1,41 @@
+import ContactUsForm from "@/components/ContactUs/ContactForm";
+
 const ContactUs = () => {
   return (
-    <div className="flex flex-col w-[895px] justify-center">
-      <div>
-        <div>
-          <h3 className="text-black text-4xl font-semibold">Contact Us</h3>
-          <div></div>
+    <div className="flex flex-col w-full justify-center items-center mb-[100px]">
+      <div className="flex w-[895px] flex-col justify-center">
+        <div className="w-full flex flex-col justify-center items-center pt-[14px] pb-[39px] gap-[30px]">
+          <div className="flex w-[624px] flex-col gap-5">
+            <h3 className="font-semibold text-4xl text-black">Contact us</h3>
+            <p className="text-borderColor">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam. Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua. Ut enim ad minim veniam
+            </p>
+          </div>
+          <div className="flex gap-[50px] py-2.5">
+            <div className="w-[294px] flex flex-col gap-2.5 pt-4 pl-4 pr-[18px] pb-[15px] border border-borderColor2 rounded-xl">
+              <h4 className="font-semibold text-2xl leading-10 text-black">
+                Address
+              </h4>
+              <p className="text-lg leading-7 text-borderColor">
+                1328 Oak Ridge Drive, Saint Louis, Missouri
+              </p>
+            </div>
+            <div className="w-[294px] flex flex-col gap-2.5 pt-4 pl-4 pr-[18px] pb-[15px] border border-borderColor2 rounded-xl">
+              <h4 className="font-semibold text-2xl leading-10 text-black">
+                Contact
+              </h4>
+              <p className="text-lg leading-7 text-borderColor">
+                313-332-8662 info@email.com
+              </p>
+            </div>
+          </div>
         </div>
       </div>
-      <div></div>
+      <ContactUsForm></ContactUsForm>
     </div>
   );
 };
---
+++ components/ContactUs/ContactForm.tsx
@@ -0,0 +1,42 @@
+const ContactUsForm = () => {
+  return (
+    <div className="pl-[35px] pt-[29px] pr-[130px] pb-[26px] bg-bgColor flex flex-col gap-6">
+      <h5 className="font-semibold text-lg leading-[26px] text-black">
+        Leave a Message
+      </h5>
+      <form className="flex flex-col gap-[29px] items-start">
+        <div className="flex flex-col gap-5">
+          <div className="flex flex-col gap-[21px]">
+            <div className="flex gap-7">
+              <input
+                type="text"
+                className="w-[225px] text-[#97989F] placeholder:text-[#97989F] pt-[5px] pl-[18px] leading-[26px] border border-[#DCDDDF] rounded-[5px] pb-[7px]"
+                placeholder="Your Name"
+              />
+              <input
+                type="text"
+                className="w-[225px] text-[#97989F] placeholder:text-[#97989F] pt-[5px] pl-[18px] leading-[26px] border border-[#DCDDDF] rounded-[5px] pb-[7px]"
+                placeholder="Your Email"
+              />
+            </div>
+            <input
+              type="text"
+              className="w-full text-[#97989F] placeholder:text-[#97989F] py-[5.5px] pl-5 pr[50px] leading-[26px] border border-[#DCDDDF] rounded-[5px]"
+              placeholder="Your Subject"
+            />
+          </div>
+          <input
+            type="text"
+            className="w-full text-[#97989F] placeholder:text-[#97989F] py-[5.5px] pl-5 pr[50px] leading-[26px] border border-[#DCDDDF] rounded-[5px]"
+            placeholder="Write a Message"
+          />
+        </div>
+        <button className="bg-voiletishBlue inline-flex  px-4 py-2.5 rounded-md font-medium text-sm text-white">
+          Send Message
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ContactUsForm;
